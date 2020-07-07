5062de3c67b76d5cc4002088d8edc7c3753145bd
lightyears1998 <lightyears1998@hotmail.com>
2020-07-07 17:08:58 +0800

    Lint code

--- src/router/token.ts
+++ src/router/token.ts
@@ -55,7 +55,7 @@ export const UserTokenHanler: express.Handler = function (req, res, next) {
         username: parsedToken.username
       };
     } catch (err) {
-      console.log(err)
+      console.log(err);
       res.status(HTTP_STATUS.UNAUTHORIZED).send("Token 无效。");
       return;
     }
