328f3901d1cd7271faadec017ebf4dd8c109389d
lightyears1998 <lightyears1998@hotmail.com>
2020-07-12 17:27:58 +0800

    Prevent scripts from running as root

--- bin/update-backend.zsh
+++ bin/update-backend.zsh
@@ -3,7 +3,26 @@
 # Update backend of QNote to the latest version on git branch.
 #
 
+USER=qnote
 PM2_PROCESS_NAME=QNote
+
+if [[ $UID == 0 || $EUID == 0 ]]; then
+    echo "Running this script as root is not allowed."
+
+    if [[ $USER == "root" ]]; then
+        exit 1
+    fi
+
+    echo "Try to run as $USER."
+
+    id -u $USER
+    if [[ $? == 1 ]]; then
+        echo "Fail to run this script as $USER, because that user $USER doesn't exist."
+        exit 1
+    fi
+
+    sudo -u $USER $0
+fi
 
 cd "$(dirname $0)/.."
 echo "Working directory: $PWD"
--- bin/update-frontend.zsh
+++ bin/update-frontend.zsh
@@ -3,6 +3,26 @@
 # Update frontend of QNote to the latest version on git branch.
 #
 
+USER=qnote
+
+if [[ $UID == 0 || $EUID == 0 ]]; then
+    echo "Running this script as root is not allowed."
+
+    if [[ $USER == "root" ]]; then
+        exit 1
+    fi
+
+    echo "Try to run as $USER."
+
+    id -u $USER
+    if [[ $? == 1 ]]; then
+        echo "Fail to run this script as $USER, because that user $USER doesn't exist."
+        exit 1
+    fi
+
+    sudo -u $USER $0
+fi
+
 BACKEND_HOME="$(dirname $0)/.."
 BACKEND_HOME="$BACKEND_HOME:A"
 
