2ebeb637ade2ef1748eb3f872274f6c5e09ad6ce
lightyears1998 <lightyears1998@hotmail.com>
2020-07-14 10:14:42 +0800

    Let's play with hierarchy

--- src/controller/base.ts
+++ src/controller/base.ts
@@ -3,15 +3,7 @@ export abstract class StaticController {
 }
 
 
-export abstract class StatefulController {
-  public abstract init(): void
+export abstract class StatefulController extends StaticController {
   public abstract start(): void
   public abstract stop(): void
-}
-
-
-export class Mail {
-  public readonly to: string;
-  public readonly subject: string;
-  public readonly htmlBody: string;
 }
